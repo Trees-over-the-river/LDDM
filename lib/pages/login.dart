01b2af3c27f39868ce9b3494f907fff514734ad9
MrNaru300 <bruno.santi.oli@gmail.com>
2023-08-12 17:38:49 -0300

    feat: flutter logo

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,68 +5,75 @@ class Login extends StatelessWidget {
 
   final _formKey = GlobalKey<FormState>();
 
+  void _validateForm(BuildContext context) {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Logado com sucesso')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Login'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.save),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/home');
-              },
-            )
-          ],
+          centerTitle: true,
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         ),
-        body: Center(
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          alignment: Alignment.center,
           child: Form(
               key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: 'Coloque seu email',
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const FlutterLogo(
+                      size: 150,
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Informe um email';
-                      } else if (!value.contains(
-                          RegExp(r'^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$'))) {
-                        return 'Email inválido';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: 'Coloque sua senha',
+                    SizedBox.fromSize(size: const Size.fromHeight(20)),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Coloque seu email',
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Informe um email';
+                        } else if (!value.contains(
+                            RegExp(r'^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$'))) {
+                          return 'Email inválido';
+                        }
+                        return null;
+                      },
+                      onFieldSubmitted: (_) => _validateForm(context),
                     ),
-                    keyboardType: TextInputType.visiblePassword,
-                    validator: (value) {
-                      if (value == null) {
-                        return 'Senha inválida';
-                      } else if (value.isEmpty) {
-                        return 'Informe uma senha';
-                      } else if (value.length < 6) {
-                        return 'Senha deve ter no mínimo 6 caracteres';
-                      }
-                      return null;
-                    },
-                    obscureText: true,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Logado com sucesso')),
-                        );
-                      }
-                    },
-                    child: const Text('Login'),
-                  ),
-                ],
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Coloque sua senha',
+                      ),
+                      keyboardType: TextInputType.visiblePassword,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Informe uma senha';
+                        } else if (value.length < 6) {
+                          return 'Senha deve ter no mínimo 6 caracteres';
+                        }
+                        return null;
+                      },
+                      onFieldSubmitted: (_) => _validateForm(context),
+                      obscureText: true,
+                    ),
+                    SizedBox.fromSize(size: const Size.fromHeight(20)),
+                    ElevatedButton(
+                      onPressed: () => _validateForm(context),
+                      child: const Text('Login'),
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                  ],
+                ),
               )),
         ));
   }
