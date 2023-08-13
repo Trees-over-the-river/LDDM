55b9435ccb3e3c4367f996d3fa0646056cca60eb
mrnaru300 <bruno.santi.oli@gmail.com>
2023-08-13 16:57:36 -0300

    feat: cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/login': (context) => Login(),
+        '/login': (context) => const Login(),
       },
       home: const MyHomePage(title: 'Pricely'),
     );
@@ -35,18 +35,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool _initialized = false;
-
   @override
   Widget build(BuildContext context) {
-    if (!_initialized) {
-      Future.delayed(const Duration(seconds: 3), () {
-        setState(() {
-          _initialized = true;
-        });
-      });
-    }
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -56,15 +46,13 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Hero(
+            const Hero(
               tag: 'Logo',
               child: FlutterLogo(
                 size: 150,
                 curve: Curves.easeInOut,
-                duration: const Duration(seconds: 1),
-                style: _initialized
-                    ? FlutterLogoStyle.markOnly
-                    : FlutterLogoStyle.horizontal,
+                duration: Duration(seconds: 1),
+                style: FlutterLogoStyle.markOnly,
               ),
             ),
             SizedBox.fromSize(size: const Size.fromHeight(50)),
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
-  Login({Key? key}) : super(key: key);
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
 
-  final _formKey = GlobalKey<FormState>();
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>(debugLabel: "Login Form");
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  bool _signUp = false;
 
   void _validateForm(BuildContext context) {
     if (_formKey.currentState!.validate()) {
@@ -18,6 +27,10 @@ class Login extends StatelessWidget {
         context,
         '/',
         (route) => false,
+        arguments: {
+          'email': _emailController.text,
+          'password': _passwordController.text,
+        },
       );
     }
   }
@@ -26,76 +39,126 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Login'),
+          title: Text(_signUp ? 'Cadastro' : 'Login'),
           centerTitle: true,
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         ),
-        body: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          alignment: Alignment.center,
-          child: SingleChildScrollView(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Theme.of(context).colorScheme.primaryContainer,
-              ),
-              constraints: const BoxConstraints(maxWidth: 800),
-              padding: const EdgeInsets.all(20),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Hero(
-                      tag: 'Logo',
-                      child: FlutterLogo(
-                        size: 150,
-                        style: FlutterLogoStyle.markOnly,
-                      ),
-                    ),
-                    SizedBox.fromSize(size: const Size.fromHeight(20)),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: 'Coloque seu email',
-                      ),
-                      keyboardType: TextInputType.emailAddress,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Informe um email';
-                        } else if (!value.contains(
-                            RegExp(r'^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$'))) {
-                          return 'Email inválido';
-                        }
-                        return null;
-                      },
-                      onFieldSubmitted: (_) => _validateForm(context),
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: 'Coloque sua senha',
-                      ),
-                      keyboardType: TextInputType.visiblePassword,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Informe uma senha';
-                        } else if (value.length < 6) {
-                          return 'Senha deve ter no mínimo 6 caracteres';
-                        }
-                        return null;
-                      },
-                      onFieldSubmitted: (_) => _validateForm(context),
-                      obscureText: true,
-                    ),
-                    SizedBox.fromSize(size: const Size.fromHeight(20)),
-                    ElevatedButton(
-                      onPressed: () => _validateForm(context),
-                      child: const Text('Login'),
-                    ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-                  ],
+        body: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Theme.of(context).colorScheme.primaryContainer,
+            ),
+            constraints: const BoxConstraints(maxWidth: 800),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+            alignment: Alignment.center,
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Hero(
+                  tag: 'Logo',
+                  child: FlutterLogo(
+                    size: 150,
+                    style: FlutterLogoStyle.markOnly,
+                  ),
                 ),
-              ),
+                SizedBox.fromSize(size: const Size.fromHeight(50)),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Coloque seu email',
+                        ),
+                        controller: _emailController,
+                        autofocus: true,
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Informe um email';
+                          } else if (!value.contains(
+                              RegExp(r'^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$'))) {
+                            return 'Email inválido';
+                          }
+                          return null;
+                        },
+                        onFieldSubmitted: (_) => _validateForm(context),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Coloque sua senha',
+                        ),
+                        keyboardType: TextInputType.visiblePassword,
+                        controller: _passwordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Informe uma senha';
+                          } else if (value.length < 6) {
+                            return 'Senha deve ter no mínimo 6 caracteres';
+                          }
+                          return null;
+                        },
+                        onFieldSubmitted: (_) => _validateForm(context),
+                        obscureText: true,
+                      ),
+                      AnimatedCrossFade(
+                        crossFadeState: _signUp
+                            ? CrossFadeState.showSecond
+                            : CrossFadeState.showFirst,
+                        firstChild: const SizedBox.shrink(),
+                        sizeCurve: Curves.easeInOut,
+                        duration: const Duration(milliseconds: 300),
+                        secondChild: TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: 'Confirme sua senha',
+                          ),
+                          keyboardType: TextInputType.visiblePassword,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Informe uma senha';
+                            } else if (value.length < 6) {
+                              return 'Senha deve ter no mínimo 6 caracteres';
+                            } else if (value != _passwordController.text) {
+                              return 'Senhas não conferem';
+                            }
+                            return null;
+                          },
+                          onFieldSubmitted: (_) => _validateForm(context),
+                          obscureText: true,
+                        ),
+                      ),
+                      SizedBox.fromSize(size: const Size.fromHeight(20)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _signUp = !_signUp;
+                              });
+                            },
+                            child: Text(
+                              _signUp
+                                  ? 'Já tem conta? Login'
+                                  : 'Não tem conta? Cadastre-se',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.secondary,
+                              ),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () => _validateForm(context),
+                            child: Text(_signUp ? 'Cadastrar' : 'Entrar'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ));
