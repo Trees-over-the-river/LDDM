71b17c9b16f2ccd90945a1acd906915d3c053a0e
MrNaru300 <bruno.santi.oli@gmail.com>
2023-09-18 09:55:58 -0300

    fix: lists_pages not reordering

--- lib/pages/lists_page.dart
+++ lib/pages/lists_page.dart
@@ -15,34 +15,34 @@ class ListsPage extends StatefulWidget {
 }
 
 class _ListsPageState extends State<ListsPage> {
-  List<List<Item>> items = List.generate(
+  List<Widget> tiles = List.generate(
     10,
-    (listi) => List.generate(
-      100,
-      (index) => Item((index + listi * 100).toString(),
-          name: 'Item ${index + listi * 100}',
-          amount: Random().nextInt(1000),
-          amountUnit: AmountUnit.none,
-          image: CachedNetworkImageProvider(
-              'https://picsum.photos/seed/${index + listi * 100}/200/200',
-              cacheKey: 'item_${index + listi * 100}'),
-          category: List.generate(
-            Random().nextInt(5),
-            (index) => 'Category $index',
-          ),
-          description: 'Description ${index + listi * 100}'),
+    (listi) => SizedBox(
+      width: 200,
+      height: 200,
+      child: ListGriditemWidget(
+        List.generate(
+          100,
+          (index) => Item((index + listi * 100).toString(),
+              name: 'Item ${index + listi * 100}',
+              amount: Random().nextInt(1000),
+              amountUnit: AmountUnit.none,
+              image: CachedNetworkImageProvider(
+                  'https://picsum.photos/seed/${index + listi * 100}/200/200',
+                  cacheKey: 'item_${index + listi * 100}'),
+              category: List.generate(
+                Random().nextInt(5),
+                (index) => 'Category $index',
+              ),
+              description: 'Description ${index + listi * 100}'),
+        ),
+        title: "List $listi",
+      ),
     ),
   );
 
   @override
   Widget build(BuildContext context) {
-    var tiles = [
-      for (int i = 0; i < items.length; i++)
-        SizedBox.square(
-            dimension: 180,
-            child: ListGriditemWidget(items[i], title: 'List $i'))
-    ];
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -75,8 +75,8 @@ class _ListsPageState extends State<ListsPage> {
       if (oldIndex < newIndex) {
         newIndex -= 1;
       }
-      final item = items.removeAt(oldIndex);
-      items.insert(newIndex, item);
+      final item = tiles.removeAt(oldIndex);
+      tiles.insert(newIndex, item);
     });
   }
 }
